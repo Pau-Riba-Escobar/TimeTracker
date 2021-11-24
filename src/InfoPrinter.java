f1f4431dc2ad43e863f2e8fb2a6b3eea03285c91
unknown <marc.rodriguezcan@e-campus.uab.cat>
2021-11-24 02:54:16 +0100

    Checkstyle

--- src/InfoPrinter.java
+++ src/InfoPrinter.java
@@ -5,28 +5,49 @@ import org.slf4j.MarkerFactory;
 
 /**
  * <h2>Clase {@code InfoPrinter}</h2>
- * Implementación de la interfaz {@code Visitor} que nos permite imprimir la información de los objetos de
+ * Implementación de la interfaz {@code Visitor}
+ * que nos permite imprimir la información de los objetos de
  * las clases {@code Task, Project y Interval}
  */
-public class InfoPrinter implements Visitor{
+public class InfoPrinter implements Visitor {
   private static final Logger logger = LoggerFactory.getLogger("InfoPrinter");
   private static final Marker fita1 = MarkerFactory.getMarker("F1");
   private static final Marker fita2 = MarkerFactory.getMarker("F2");
+
   @Override
   public void visit(Task task) {
-    logger.debug(fita2,"Task "+task.getName()+" child of "+task.getParentProject().getName()+" created correctly \n");
-    logger.info(fita1,"Task "+task.getName()+" child of "+task.getParentProject().getName()+"  "+task.getInitialDateTime()+"  "+task.getFinalDateTime()+"  "+task.getDuration().toSeconds()+"\n");
+    logger.debug(fita2, "Task " + task.getName()
+            + " child of " + task.getParentProject().getName()
+            + " created correctly \n");
+    logger.info(fita1, "Task " + task.getName()
+            + " child of " + task.getParentProject().getName() + "  "
+            + task.getInitialDateTime() + "  " + task.getFinalDateTime()
+            + "  " + task.getDuration().toSeconds() + "\n");
   }
 
   @Override
   public void visit(Project project) {
-    logger.debug(fita2,"Project "+project.getName()+" child of "+((project.getParentProject()!=null)?project.getParentProject().getName():"null")+" created correctly \n");
-    logger.info(fita1,"Project "+project.getName()+" child of "+((project.getParentProject()!=null)?project.getParentProject().getName():"null")+"  "+project.getInitialDateTime()+"  "+project.getFinalDateTime()+"  "+project.getDuration().toSeconds()+"\n");
+    logger.debug(fita2, "Project " + project.getName()
+            + " child of "
+            + ((project.getParentProject() != null)
+            ? project.getParentProject().getName() : "null") + " created correctly \n");
+    logger.info(fita1, "Project " + project.getName()
+            + " child of " + ((project.getParentProject() != null)
+            ? project.getParentProject().getName() : "null")
+            + "  " + project.getInitialDateTime() + "  " + project.getFinalDateTime()
+            + "  " + project.getDuration().toSeconds() + "\n");
   }
 
   @Override
   public void visit(Interval interval) {
-    logger.debug(fita2,"Interval child of "+interval.getParentTask().getName()+"  "+interval.getStart()+"  "+interval.getEnd()+"  "+interval.getDuration().toSeconds()+" created correctly \n");
-    logger.info(fita1,"Interval child of "+interval.getParentTask().getName()+"  "+interval.getStart()+"  "+interval.getEnd()+"  "+interval.getDuration().toSeconds()+"\n");
+    logger.debug(fita2, "Interval child of "
+            + interval.getParentTask().getName() + "  "
+            + interval.getStart() + "  " + interval.getEnd()
+            + "  " + interval.getDuration().toSeconds()
+            + " created correctly \n");
+    logger.info(fita1, "Interval child of " + interval.getParentTask().getName()
+            + "  " + interval.getStart() + "  "
+            + interval.getEnd() + "  "
+            + interval.getDuration().toSeconds() + "\n");
   }
 }
